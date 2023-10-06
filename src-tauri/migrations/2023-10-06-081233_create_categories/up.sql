fb5f41d218e49a1383e293622f8760491d94254d
Jacob Mellin <mail@jacobmellin.de>
2023-10-06 11:22:26 +0200

    Update migrations

--- src-tauri/migrations/2023-10-06-081233_create_categories/up.sql
+++ src-tauri/migrations/2023-10-06-081233_create_categories/up.sql
@@ -1,6 +1,3 @@
 CREATE TABLE categories (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR NOT NULL,
-    description VARCHAR,
-    monthly_budget DECIMAL(15,3) NOT NULL DEFAULT '0'
+    id VARCHAR NOT NULL PRIMARY KEY
 )
