4ba81d7bc47b5a579bc778380642ed51c99acd3c
Jacob Mellin <mail@jacobmellin.de>
2023-10-09 19:57:03 +0200

    Add basic frontend for showing books

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [books, setBooks] = useState<Array<Record<string, any>>>([]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    //setGreetMsg(await invoke("greet", { name }));
-    setGreetMsg(await invoke("get_books"));
-  }
+  useEffect(() => {
+    let getBooks = async () => { 
+        let result: Array<any> = await invoke("get_books");
+        setBooks(JSON.parse(result));
+    }
+    getBooks(); 
+  }, []);
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-indigo-400 font-thin text-5xl tracking-wide mb-3">INBAN</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <main className="bg-indigo-800 border-indigo-700 border-solid border-2 mt-10 rounded-lg p-5 w-[400px]">
+        <ul className="text-indigo-200">
+            { 
+                books.map(book => (
+                    <li key="{book.id}">{ book.name }</li>
+                ))
+            }
+        </ul>
+      </main>
     </div>
   );
 }
